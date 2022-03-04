7f4127b2a8975f9d89921cb1ebb19e09daef60ff
achmadkonde <achmadrasidi@gmail.com>
2022-03-04 21:16:28 +0700

    Create server.js

---
+++ backEndNodeJS/shortUrl/server.js
@@ -0,0 +1,61 @@
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    require: true,
+  },
+  short_url: {
+    type: String,
+    require: true,
+    default: shortid.generate,
+  },
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
+app.use(cors());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+app.post("/api/shorturl", async (req, res) => {
+  const url = await Url.create({ original_url: req.body.url });
+  if (!validUrl.isUri(url.original_url)) {
+    res.json({ error: "invalid url" });
+  } else {
+    res.json({ original_url: url.original_url, short_url: url.short_url });
+  }
+});
+
+app.get("/api/shorturl/:shorturl", async (req, res) => {
+  const shortUrl = req.params.shorturl;
+  const url = await Url.findOne({ short_url: shortUrl });
+  try {
+    res.redirect(url.original_url);
+  } catch (error) {
+    res.json({ error: "invalid url" });
+  }
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
