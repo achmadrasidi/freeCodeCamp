a375c1d5eb23bd3ecdd8eb557e815625d91e67dc
Achmad Rasidi <achmadrasidi@gmail.com>
2022-03-06 00:06:17 +0700

    Create server.js

---
+++ backEndNodeJS/fileMetaData/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "./public/data/uploads/" });
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+  try {
+    res.json({ name: file.originalname, type: file.mimetype, size: file.size });
+  } catch (error) {
+    res.json({ error: "file not found" });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
