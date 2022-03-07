ce0af16467a1342e913fb81416f12d3ff29f09f8
Achmad Rasidi <achmadrasidi@gmail.com>
2022-03-07 18:21:32 +0700

    Add files via upload

---
+++ frontEnd/React/create-a-complex-jsx-element.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+<h1>Hello JSX!</h1>
+<p>this is react JSX</p>
+<ul>
+<li>item1</li>
+<li>item1</li>
+<li>item1</li>
+</ul>
+</div>
---
+++ frontEnd/React/give-sibling-elements-a-unique-key-attribute.js
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((key)=>{
+    return <li key={key}>{key}</li>
+  }); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
